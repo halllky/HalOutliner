7ee2258e7f59aa07478909d641333818ba6d91cd
halky <yuyami.stars@gmail.com>
2018-12-24 11:20:08 +0900

    Use Vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import 'es6-promise/auto'
 import App from './components/MemoRoot'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+const store = new Vuex.Store({})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   components: { App },
   template: '<App/>'
 })
