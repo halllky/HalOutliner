62eed3e841e042fe5b5359223a5bd3036a35b104
halky <yuyami.stars@gmail.com>
2018-12-30 16:18:15 +0900

    Add 'HalOutliner.vue'

---
+++ src/components/HalOutliner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <memo-root></memo-root>
+  </div>
+</template>
+<script>
+import MemoRoot from './MemoRoot.vue';
+export default {
+  components: { MemoRoot }
+}
+</script>
