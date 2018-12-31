c7a5bc54c15bc6d78d1d57a23220fd994ef08505
halky <yuyami.stars@gmail.com>
2018-12-31 21:33:25 +0900

    Move footer menu icons to FooterMenu.vue

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -40,4 +40,58 @@ export default {
     margin: 0 2px;
   }
 }
+/* https://cssicon.space/#/ */
+.magnifier {
+  color: $col_main;
+  position: absolute;
+  margin-top: 1px;
+  margin-left: 2px;
+  width: 12px;
+  height: 12px;
+  border: solid 2px currentColor;
+  border-radius: 100%;
+  -webkit-transform: rotate(-45deg);
+          transform: rotate(-45deg);
+}
+.magnifier:before {
+  content: '';
+  position: absolute;
+  top: 12px;
+  left: 5px;
+  height: 8px;
+  width: 2px;
+  background-color: currentColor;
+}
+.download {
+  color: $col_main;
+  position: absolute;
+  margin-left: 0px;
+  margin-top: 6px;
+  width: 13px;
+  height: 4px;
+  border-radius: 2px;
+  border: solid 2px currentColor;
+  border-top: none;
+}
+.download:before {
+  content: '';
+  position: absolute;
+  left: 6px;
+  top: -9px;
+  width: 2px;
+  height: 10px;
+  background-color: currentColor;
+}
+.download:after {
+  content: '';
+  position: absolute;
+  left: 4px;
+  top: -4px;
+  width: 4px;
+  height: 4px;
+  border-top: solid 2px currentColor;
+  border-right: solid 2px currentColor;
+  -webkit-transform: rotate(135deg);
+          transform: rotate(135deg);
+}
 </style>
