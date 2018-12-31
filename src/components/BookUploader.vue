4f70df8791d0db4c56e79809de13cdd0fd59a4ea
halky <yuyami.stars@gmail.com>
2018-12-31 21:06:48 +0900

    Add BookUploader design

--- src/components/BookUploader.vue
+++ src/components/BookUploader.vue
@@ -1,5 +1,11 @@
 <template>
-  <dialog ref="dialog">
+  <dialog class="book-uploader" ref="dialog">
+    <p class="txt">please paste JSON here to upload your memo</p>
+    <textarea
+      class="book-uploader__txt"
+      spellcheck="false"
+      onfocus="select()"/>
+    <a class="book-uploader__ok">OK</a>
   </dialog>
 </template>
 <script>
@@ -9,3 +15,22 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+@import '../assets/util.scss';
+.book-uploader{
+  border: none;
+  border-radius: $siz_radius;
+  background: $col_main;
+  &__txt{
+    @extend .txt;
+    resize: none;
+    border: solid 1px;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  &__ok{
+    @extend .btn;
+    min-width: 5em;
+  }
+}
+</style>
