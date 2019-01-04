357cf7cafa3576b081aecb92576c7489fc1ec924
halky <yuyami.stars@gmail.com>
2019-01-04 22:16:08 +0900

    Add downloading for android

--- src/assets/util.js
+++ src/assets/util.js
@@ -1,7 +1,42 @@
+/* eslint-disable no-undef */
 export default {
   methods: {
     isAndroid () {
       return typeof cordova !== 'undefined';
+    },
+    downloadForAndroid (blob, fileName) {
+      function execDownload () {
+        const fr = new FileReader();
+        fr.onload = () => {
+          new FileTransfer().download(
+            fr.result,
+            cordova.file.externalRootDirectory + 'Download/' + fileName,
+            function (fileEntry) {
+              alert('download complete to ' + fileEntry.fullPath);
+            },
+            function (error) {
+              alert('download error code: ' + error.code);
+            }
+          );
+        }
+        fr.readAsDataURL(blob);
+      }
+      const per = cordova.plugins.permissions;
+      per.checkPermission(
+        per.WRITE_EXTERNAL_STORAGE,
+        function (statusBeforeRequest) {
+          if (statusBeforeRequest.hasPermission) {
+            execDownload();
+          } else {
+            per.requestPermission(
+              per.WRITE_EXTERNAL_STORAGE,
+              function (statusAfterRequest) {
+                if (statusAfterRequest.hasPermission) execDownload();
+              }
+            );
+          }
+        }
+      );
     }
   }
 }
