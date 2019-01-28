d07261d8507b58d319c5bddf268c78bef63efdcf
halky <yuyami.stars@gmail.com>
2019-01-28 21:44:42 +0900

    Add store.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './components/HalOutliner'
 import Util from './assets/util'
+import MyStore from './assets/store'
 import Vuex from 'vuex'
 import 'es6-promise/auto'
 
@@ -10,7 +11,7 @@ Vue.config.productionTip = false
 Vue.mixin(Util)
 
 Vue.use(Vuex)
-const store = new Vuex.Store({})
+const store = new Vuex.Store(MyStore)
 
 /* eslint-disable no-new */
 new Vue({
